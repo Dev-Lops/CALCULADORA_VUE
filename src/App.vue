acafc68fdcee76f415e572ca4a748e48d09b216a
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 00:34:39 -0400

    🚀 Projeto Finalizado

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- App.vue -->
+<template>
+  <div id="app" class="container">
+
+    <Calculadora />
+
+  </div>
+</template>
+
+<script>
+import Calculadora from './components/Calculadora.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
+</script>
+
+
